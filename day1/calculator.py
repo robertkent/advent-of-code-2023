dd5f3ef57ad087c4bb5a25c59e0a6f1dcf2f27d9
Robert Kent <robert.kent1@vodafone.com>
2023-12-01 15:37:19 +0000

    Refactoring a little

--- day1/calculator.py
+++ day1/calculator.py
@@ -1,35 +1,26 @@
 import re
 f = open("puzzleInput.txt", "r")
-fo = open("puzzleOutput.txt", "w")
 numWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 regEx = r'(\d+)|(?=(' + '|'.join(numWords) + '))'
-
-print(regEx);
 
 def reformatNumbers(nums):
     newNumbers = []
     for x in nums:
-        if x in numWords:
-            newNumbers.append(str(numWords.index(x) + 1))
-        else:
-            newNumbers.append(x)
+        newNumbers.append(str(numWords.index(x) + 1)) if x in numWords else newNumbers.append(x)
     
     return newNumbers
 
 def getCalibration():
     calibrationNumbers = []
-    for index, x in enumerate(f):
-        nums = re.findall(regEx, x, re.DOTALL)
+    for x in f:
+        nums = re.findall(regEx, x)
         result = [match[0] or match[1] for match in nums if any(match)]
-        reformattedNumbers = reformatNumbers(result)
-        numString = ''.join(reformattedNumbers)
+        numString = ''.join(reformatNumbers(result))
         calibration = numString[0] + numString[-1]
         calibrationNumbers.append(int(calibration))
-        fo.write(f'{x}={calibration}\n')
 
     return sum(calibrationNumbers)
 
 print(getCalibration())
 
 f.close()
-fo.close()
